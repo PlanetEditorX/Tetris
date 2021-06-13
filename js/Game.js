8d24460c0e2f19d46eb01256b495f14a4c681c90
XiongYinJ <962129122@qq.com>
2021-06-13 14:20:43 +0800

    03.方块渲染

--- js/Game.js
+++ js/Game.js
@@ -6,7 +6,7 @@
         //初始化
         this.init()
     } 
-    
+    //初始化布局
     Game.prototype.init = function () {
         var $table = $("<table></table>")
         //渲染表格
@@ -24,5 +24,16 @@
         }
         //将表格追加到body
         $($table).appendTo("body")
+    }
+
+    /**
+     * 方块渲染
+     * @param {int} row 行
+     * @param {int} col 列
+     * @param {int} num 状态
+     */
+    Game.prototype.setColor = function (row,col,num) {
+        // 给对应的有颜色方块添加类名
+        $("tr").eq(row).children("td").eq(col).addClass("c"+num)
     }
 })()
